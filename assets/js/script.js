97b9ba8c869e78c6614088c45ff205527b3f5582
JosephJoeyJoe <88215210+JosephJoeyJoe@users.noreply.github.com>
2021-11-20 22:15:22 -0500

    print local storage back to page and refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -220,6 +220,21 @@ localStorage.setItem("tasks", JSON.stringify(tasks));
 
 }
 
+var loadTasks = function() {
+  var savedTasks = localStorage.getItem("tasks");
+
+  if (!savedTasks) {
+    return false;
+  }
+
+  savedTasks = JSON.parse(savedTasks);
+  // loop through savedTasks array
+for (var i = 0; i < savedTasks.length; i++) {
+  // pass each task object into the `createTaskEl()` function
+  createTaskEl(savedTasks[i]);
+}
+};
+
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -228,3 +243,4 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 // for changing the status
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+loadTasks();
